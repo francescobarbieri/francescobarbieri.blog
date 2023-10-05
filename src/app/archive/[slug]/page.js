3d2e6d276b8b2b3c209f9a902a9eec85be47496d
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-10-05 21:35:45 +0200

    generate static metadata with basic fields

--- src/app/archive/[slug]/page.js
+++ src/app/archive/[slug]/page.js
@@ -34,3 +34,50 @@ export async function generateStaticParams() {
         slug: post,
     }))
 }
+
+export async function generateMetadata({params}, parent) {
+    const id = params.slug;
+    const post = await getSinglePost(id);
+
+    return {
+        title: post.title,
+        description: 'About me page.',
+        alternates: {
+            canonical: '/archive/' + post.link,
+            languages: {
+            'en-US': '/en-US',
+            },
+        },
+        openGraph: {
+            title: post.title + " - Francesco Barbieri's Blog",
+            description: 'About me page.',
+            url: 'https://francescobarbieri.blog/archive/' + post.link,
+            siteName: "Francesco Barbieri's Blog",
+            locale: 'en_US',
+            /*
+            images: [
+            {
+                url: 'https://nextjs.org/og.png',
+                width: 800,
+                height: 600,
+            },
+            ],
+            */
+            type: 'website'
+        },
+        /*
+        twitter: {
+            card: 'summary_large_image',
+            title: 'Next.js',
+            description: 'The React Framework for the Web',
+            siteId: '1467726470533754880',
+            creator: '@nextjs',
+            creatorId: '1467726470533754880',
+            images: ['https://nextjs.org/og.png'],
+        },
+        */
+        themeColor: 'light',
+        archives: ['https://francescobarbieri.blog/archive'],
+        category: post.tag,
+    }
+}
