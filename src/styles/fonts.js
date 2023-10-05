4cdeb3b96e61cfee0ff8f8e0ecabc9e5b168b05c
Edmond Chui <1967998+EdmondChuiHW@users.noreply.github.com>
2023-10-05 20:03:22 +0100

    Create fonts.js

---
+++ src/styles/fonts.js
@@ -0,0 +1,4 @@
+import { Roboto_Slab, Source_Sans_3 } from 'next/font/google';
+
+export const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
+export const robotoSlab = Roboto_Slab({ subsets: ['latin'] });
