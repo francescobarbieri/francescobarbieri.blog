8b53b9653af1e5d46bc84986266b5dfadbedaa8e
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-10-08 12:20:45 +0200

    reading list page metadata

--- src/app/readings/page.js
+++ src/app/readings/page.js
@@ -2,8 +2,43 @@ import SidebarButton from '@/components/SidebarButton'
 import Readings from '@/pages/Readings';
 
 export const metadata = {
-  title: 'Readings - Francesco Barbieri',
-  description: 'Blogging about tech, finance and coding.',
+    title: 'Reading List',
+    description: 'Recommended readings.',
+    alternates: {
+      canonical: '/readings',
+      languages: {
+        'en-US': '/en-US',
+      },
+    },
+    openGraph: {
+      title: "Reading List - Francesco Barbieri's Blog.",
+      description: 'Recommended readings',
+      url: 'https://francescobarbieri.blog/readings',
+      siteName: "Francesco Barbieri's Blog",
+      locale: 'en_US',
+      images: [
+        {
+          url: 'https://francescobarbieri.blog/og/og.png',
+          width: 800,
+          height: 400,
+        },
+      ],
+      type: 'website'
+    },
+    /*
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Next.js',
+      description: 'The React Framework for the Web',
+      siteId: '1467726470533754880',
+      creator: '@nextjs',
+      creatorId: '1467726470533754880',
+      images: ['https://nextjs.org/og.png'],
+    },
+    */
+    themeColor: 'light',
+    archives: ['https://francescobarbieri.blog/archive'],
+    category: 'technology',
 }
 
 export default async function Finance() {
