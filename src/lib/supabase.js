1c49b7770e30239986e08c070f68c8072b294e85
Francesco Barbieri <fbarbieri.business@gmail.com>
2024-02-10 11:49:00 +0100

    updated supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,12 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-if (
-    process.env.SUPABASE_PROJECT_URL === '' ||
-    process.env.SUPABASE_API_KEY_PUBLIC === ''
-) {
-    throw new Error('Missing env vars SUPABASE_PROJECT_URL or SUPABASE_API_KEY_PUBLIC');
-}
-
 const supabase = createClient('https://kkqrqcujuhhtuotxmwfj.supabase.co', 
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrcXJxY3VqdWhodHVvdHhtd2ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY2MzYzMjMsImV4cCI6MjAyMjIxMjMyM30.w5olIUwrM52rXqlRs1zzgkrNjFyacw88wCwS3b6sglY');
 
